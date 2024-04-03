ab098849b261f4f08e3536c2931576fb73e806ba
CubeSuite <cubesuite@outlook.com>
2024-04-03 22:27:57 +0100

    Add project files.

---
+++ VoidChests/Patches/ChestDefinitionPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidChests.Patches
+{
+    internal class ChestDefinitionPatch
+    {
+        [HarmonyPatch(typeof(ChestDefinition), "InitInstance")]
+        [HarmonyPostfix]
+        static void setLimit(ChestDefinition __instance, ref ChestInstance newInstance) {
+            newInstance.restrictInputToSlots = 56;
+        }
+    }
+}
---
+++ VoidChests/VoidChestsPlugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using VoidChests.Patches;
+
+namespace VoidChests
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class VoidChestsPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.VoidChests";
+        private const string PluginName = "VoidChests";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake() {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+
+            Harmony.CreateAndPatchAll(typeof(InserterInstancePatch));
+            Harmony.CreateAndPatchAll(typeof(InventoryNavigatorPatch));
+            Harmony.CreateAndPatchAll(typeof(ChestDefinitionPatch));
+
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+        }
+
+        private void Update() {
+            // ToDo: Delete If Not Needed
+        }
+    }
+}
