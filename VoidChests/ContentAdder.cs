3d0ab06e8c35ba02a50fc52789e8b8cec36765d0
Elliot <cubesuite@outlook.com>
2024-11-19 08:42:40 +0000

    Updated to game version 1.0

---
+++ VoidChests/ContentAdder.cs
@@ -0,0 +1,62 @@
+﻿using EquinoxsModUtils;
+using EquinoxsModUtils.Additions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VoidChests
+{
+    public static class ContentAdder
+    {
+        public static void AddVoidChest() {
+            NewResourceDetails details = new NewResourceDetails() {
+                name = "Void Chest",
+                description = "Voids all items inserted into it",
+                craftingMethod = CraftingMethod.Assembler,
+                craftTierRequired = 0,
+                headerTitle = "Logistics",
+                subHeaderTitle = "Utility",
+                maxStackCount = 50,
+                sortPriority = 998,
+                unlockName = EMU.Names.Unlocks.BasicLogistics,
+                parentName = EMU.Names.Resources.Container,
+                sprite = EMU.Images.LoadSpriteFromFile("VoidChests.Images.VoidChest.png")
+            };
+
+            ChestDefinition definition;
+            definition = ScriptableObject.CreateInstance<ChestDefinition>();
+            definition.inventorySizes = new List<Vector2Int>() { new Vector2Int(1, 1) };
+            definition.numberOfSlots = 1;
+
+            EMUAdditions.AddNewMachine(definition, details);
+
+            EMUAdditions.AddNewRecipe(new NewRecipeDetails() {
+                GUID = VoidChestsPlugin.MyGUID,
+                craftingMethod = CraftingMethod.Assembler,
+                craftTierRequired = 0,
+                duration = 0.1f,
+                ingredients = new List<RecipeResourceInfo>() {
+                    new RecipeResourceInfo() {
+                        name = EMU.Names.Resources.IronComponents,
+                        quantity = 8
+                    },
+                    new RecipeResourceInfo() {
+                        name = EMU.Names.Resources.IronIngot,
+                        quantity = 6
+                    }
+                },
+                outputs = new List<RecipeResourceInfo>() {
+                    new RecipeResourceInfo() {
+                        name = "Void Chest",
+                        quantity = 1
+                    }
+                },
+                sortPriority = 10,
+                unlockName = EMU.Names.Unlocks.BasicLogistics
+            });
+        }
+    }
+}
---
+++ VoidChests/Patches/MachineDefinition.cs
@@ -0,0 +1,78 @@
+﻿using EquinoxsDebuggingTools;
+using EquinoxsModUtils;
+using FluffyUnderware.DevTools.Extensions;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VoidChests.Patches
+{
+    internal class MachineDefinitionPatch
+    {
+        public static GameObject prefab;
+        private static Dictionary<uint, GameObject> visualsMap = new Dictionary<uint, GameObject>();
+
+        [HarmonyPatch(typeof(MachineDefinition<ChestInstance, ChestDefinition>), nameof(MachineDefinition<ChestInstance, ChestDefinition>.OnBuild))]
+        [HarmonyPostfix]
+        static void AddToVoidChestsList(MachineInstanceRef<ChestInstance> instRef) {
+            if (instRef.Get().myDef.displayName != "Void Chest") return;
+
+            VoidChestsPlugin.voidChestIDs.Add(instRef.GetCommonInfo().instanceId);
+            EDT.Log("General", $"Added chest #{instRef.GetCommonInfo().instanceId} to voidChestIDs");
+
+            if (instRef.gridInfo.strata == GameState.instance.GetStrata()) {
+                GameObject visuals = GameObject.Instantiate(prefab, instRef.gridInfo.BottomCenter, Quaternion.Euler(0, instRef.gridInfo.yawRot, 0));
+                visuals.transform.localScale = new Vector3(1.01f, 1.01f, 1.01f);
+
+                if (visualsMap.ContainsKey(instRef.instanceId)) {
+                    GameObject.Destroy(visualsMap[instRef.instanceId]);
+                }
+
+                visualsMap[instRef.instanceId] = visuals;
+            }
+        }
+            
+        [HarmonyPatch(typeof(MachineDefinition<ChestInstance, ChestDefinition>), "OnDeconstruct")]
+        [HarmonyPrefix]
+        static void RemoveVisuals(ref InserterInstance erasedInstance) {
+            if (erasedInstance.myDef.displayName != "Void Chest") return;
+            GameObject.Destroy(visualsMap[erasedInstance.commonInfo.instanceId]);
+            visualsMap.Remove(erasedInstance.commonInfo.instanceId);
+        }
+
+        public static void RedoVisualsOnStrataChanged() {
+            if (!EMU.LoadingStates.hasGameLoaded) return;
+            ClearVisuals();
+            foreach (uint id in VoidChestsPlugin.voidChestIDs) {
+                if (!MachineManager.instance.GetRefFromId(id, out IMachineInstanceRef instRef)) continue;
+
+                if (instRef.gridInfo.strata == GameState.instance.GetStrata()) {
+                    GameObject visuals = GameObject.Instantiate(prefab, instRef.gridInfo.BottomCenter, Quaternion.Euler(0, instRef.gridInfo.yawRot, 0));
+                    visuals.transform.localScale = new Vector3(1.01f, 1.01f, 1.01f);
+
+                    if (visualsMap.ContainsKey(instRef.instanceId)) {
+                        GameObject.Destroy(visualsMap[instRef.instanceId]);
+                    }
+
+                    visualsMap[instRef.instanceId] = visuals;
+                }
+            }
+        }
+
+        internal static void OnGameUnloaded() {
+            ClearVisuals();
+        }
+
+        private static void ClearVisuals() {
+            foreach (GameObject visuals in visualsMap.Values) {
+                GameObject.Destroy(visuals);
+            }
+
+            visualsMap.Clear();
+        }
+    }
+}
